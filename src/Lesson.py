c38f0c5d17c8a5f4410245c7b9bf53e1e7c3a399
TheQChan <97827141+TheQChan@users.noreply.github.com>
2025-09-15 20:12:39 +0300

    Update Lesson.py

--- src/Lesson.py
+++ src/Lesson.py
@@ -1,12 +1,26 @@
 class Lesson:
-    def __init__(self, number: int, start_time: str, end_time: str, classroom: str, title: str,
-                 is_remote: bool = False) -> None:
+    def __init__(
+            self,
+            number: int,
+            start_time: str,
+            end_time: str,
+            classroom: str,
+            title: str,
+            is_remote: bool = False,
+            week_kind: int | None = None,
+            week_variant: int | None = None
+    ) -> None:
         self.number = number
         self.start_time = start_time
         self.end_time = end_time
         self.classroom = classroom
         self.title = title
         self.is_remote = is_remote
+        self.week_kind = week_kind
+        self.week_variant = week_variant
 
     def __str__(self):
-        return f"{self.number} | {self.start_time} - {self.end_time} | {self.classroom:6} | {self.title}"
+        return (
+            f"{self.number} | {self.start_time} - {self.end_time} | "
+            f"{self.classroom:6} | {self.title}"
+        )
